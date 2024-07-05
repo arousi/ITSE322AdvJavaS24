282fab70b70a594a958176630195b7dd755cea47
Sanad <a.arousi@yahoo.com>
2024-07-05 12:22:17 +0200

    Comments explaining each line of code

--- FullApplication/Client.java
+++ FullApplication/Client.java
@@ -7,8 +7,12 @@ public class Client{
     public static void main(String[] args) {
         try{
             Socket clientSock = new Socket("localhost",5555);
+            //*A socket and its port is what will connect the client 2 server */
+
+            //*Output Stream/
             PrintWriter pr = new PrintWriter(clientSock.getOutputStream());
             pr.println("Hello, Im client");
+            //*Output Stream is what we use to send data to a Server */
 
             pr.flush();
             clientSock.close();
--- FullApplication/Server.java
+++ FullApplication/Server.java
@@ -6,16 +6,27 @@ import java.io.*;
 public class Server {
     public static void main(String[] args) {
         try {
+            //*Socket */
             ServerSocket servSock = new ServerSocket(5555);
-
             Socket clientSock = servSock.accept();
+            //*Server Socket will connect to the same port in Client file */
             System.out.println("\nClient connected to me!\n");
+            //prints after .accept(); becomes true
 
+            //*Input Stream */
             InputStreamReader in = new InputStreamReader(clientSock.getInputStream());
-            BufferedReader bufRead = new BufferedReader(in);
+            //*Input Stream reads from Output Stream init in the Client */
 
+            //*Buffer then takes the input from Istream to process it */
+            BufferedReader bufRead = new BufferedReader(in);
             String str = bufRead.readLine();
+            //*str to store what the Buffere has read */
+
+            //Print the mssg sent from Client, OutStream-Client then InputStream-Server
+            //then Buffer-Server then str<-Buffer-Server
+
             System.out.println("Client says: " + str);
+            //Print Client msg
             
             servSock.close();
             clientSock.close();
