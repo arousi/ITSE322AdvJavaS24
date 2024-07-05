c432cf76b5890a7339e5272807e73cba98b14d72
Sanad <a.arousi@yahoo.com>
2024-07-05 13:24:21 +0200

    IO from server to client and vice versa, with some comments

--- FullApplication/Client.java
+++ FullApplication/Client.java
@@ -10,11 +10,19 @@ public class Client{
             //*A socket and its port is what will connect the client 2 server */
 
             //*Output Stream/
-            PrintWriter pr = new PrintWriter(clientSock.getOutputStream());
+            PrintWriter pr = new PrintWriter(clientSock.getOutputStream(), true);
             pr.println("Hello, Im client");
             //*Output Stream is what we use to send data to a Server */
 
-            pr.flush();
+            InputStreamReader in = new InputStreamReader(clientSock.getInputStream());
+            BufferedReader bufRead = new BufferedReader(in);
+            String inStr = bufRead.readLine();
+            System.out.println("Servre says to Client:"+inStr);
+
+            bufRead.close();//!Close Buffer 1st
+            in.close();     //!Close InputStream 2nd
+            pr.close();     //!Close OutStream 3rd
+                            //!Sockets to be closed last
             clientSock.close();
         }catch(IOException er){
             er.printStackTrace();
--- FullApplication/Server.java
+++ FullApplication/Server.java
@@ -6,38 +6,43 @@ import java.io.*;
 public class Server {
     public static void main(String[] args) {
         try {
-            //*Socket */
+            // *Socket */
             ServerSocket servSock = new ServerSocket(5555);
             Socket clientSock = servSock.accept();
-            //*Server Socket will connect to the same port in Client file */
+            // *Server Socket will connect to the same port in Client file */
             System.out.println("\nClient connected to me!\n");
-            //prints after .accept(); becomes true
+            // prints after .accept(); becomes true
 
-            //*Input Stream */
+            // *Input Stream */
             InputStreamReader in = new InputStreamReader(clientSock.getInputStream());
-            //*Input Stream reads from Output Stream init in the Client */
+            // *Input Stream reads from Output Stream init in the Client */
 
-            //*Buffer then takes the input from Istream to process it */
+            // *Buffer then takes the input from Istream to process it */
             BufferedReader bufRead = new BufferedReader(in);
             String str = bufRead.readLine();
-            //*str to store what the Buffere has read */
+            // *str to store what the Buffere has read */
 
-            //Print the mssg sent from Client, OutStream-Client then InputStream-Server
-            //then Buffer-Server then str<-Buffer-Server
+            // Print the mssg sent from Client, OutStream-Client then InputStream-Server
+            // then Buffer-Server then str<-Buffer-Server
 
             System.out.println("Client says: " + str);
-            //Print Client msg
-            
-            servSock.close();
+            // Print Client msg
+
+            PrintWriter pr = new PrintWriter(clientSock.getOutputStream(), true);
+            pr.println("Server says: Hi Im server");
+
+            bufRead.close();//!Close Buffer 1st
+            in.close();     //!Close InputStream 2nd
+            pr.close();     //!Close OutStream 3rd
+                            //!Sockets to be closed last
             clientSock.close();
+            servSock.close();
 
         } catch (Exception er) {
             er.printStackTrace();
         }
-        finally{
-
         }
 
     }
 
-}
+
